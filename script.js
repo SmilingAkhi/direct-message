3cea1c794decc0bbc8996c940053a7ac9d751d7e
Abdulrahman <bhabdulrahaman@gmail.com>
2023-09-30 14:16:30 +0100

    added the rendering function

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 "use script";
-const messagesContainer = document.querySelector("#messages");
+const messagesDisplayContainer = document.querySelector("#messages");
 const getMessage = document.querySelector("#getMessage");
 const sendMessage = document.querySelector("#send");
+
+//storing messages
+const messages = [
+  {
+    Sender: "khadija",
+    Timestamp: "1:00am",
+    Text: "okay, this is a test and i am worried if it works but oh well, lets see what happens if it gets too long would it wrap or overflow ",
+  },
+  { Sender: "Aisha", Timestamp: "1:30am", Text: "koji <3" },
+];
+
+//rending element
+function renderElement() {
+  //looping through array
+  for (message of messages) {
+    //create dom element
+    messageContainer = document.createElement("div");
+    messageContainer.classList.add("messageContainer");
+    messageContainer.innerHTML = `${message.Sender}:  ${message.Text}`;
+    console.log(messageContainer);
+    messagesDisplayContainer.appendChild(messageContainer);
+  }
+}
+renderElement();
